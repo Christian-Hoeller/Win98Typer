6c0c921888f26457be3b99fd51942d9e58f5aa4b
Christian Höller <ch.hoeller14@gmail.com>
2021-01-21 19:05:58 +0100

    check if word is correct

--- typing-test/src/components/TypingTest.vue
+++ typing-test/src/components/TypingTest.vue
@@ -1,6 +1,12 @@
 <template>
+
 <div>
     <WordBox v-bind:words="words" />
+    <div>
+        <input type="text" @input="handleKeyPress" v-model="userInput">
+        <p>current word: {{currentWord}}</p> 
+        <p>{{wordIndex}}</p>
+    </div>
 </div>
 
 </template>
@@ -16,12 +22,46 @@ export default {
     },
     data(){
         return{
-            words: this.getWords()
+            words: this.getWords(),
+            userInput: '',
+            currentWordIndex: 0,
+            currentWord: this.getWords()[0],
         }
     },
     methods: {
         getWords(){
             return json[0].words;
+        },
+        getWordByIndex(index){
+            return this.getWords()[index];
+        },
+        handleKeyPress(){
+            
+            if(this.userInput[this.userInput.length - 1] === ' '){
+                
+                let currentWord = this.getWordByIndex(this.currentWordIndex);
+                // Check if word is true
+                if(currentWord === this.userInput.split(' ')[0]){
+                    console.log("word is correct");
+                }
+                else{
+                    console.log("word is wrong")
+                }
+
+                // reset user input
+                this.userInput = '';
+
+                // inkrement currentWordIndex
+                this.currentWordIndex ++;
+
+                // inkrement currentword
+                this.currentWord = this.getWordByIndex(this.currentWordIndex);
+            }
+        }
+    },
+    computed:{
+        wordIndex() {
+            return this.currentWordIndex;
         }
     }
 }
